65c737317bfc95472f6797545f3e97c7f2df267e
bumbari <shuduev@hotmail.com>
2013-12-26 21:32:41 +0100

    added file hello.py
    Already forgotten what this code is doing...

---
+++ hello.py
@@ -0,0 +1,9 @@
+print ("Hello world")from random import randrange
+
+def censor(text, word):
+    """ """
+    l = len(word)
+    asterisks = "*" * l
+    result = text
+    while result.index(word) > -1 :
+        result = result[:index] + resultindex + l]
