39affea92c298d6f168c5941cc675b1d2dc50bb0
bumbari <shuduev@hotmail.com>
2013-12-26 21:39:49 +0100

    hello.py is modified
    comment added

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 print ("Hello world")from random import randrange
 
 def censor(text, word):
-    """ """
+    """ note, that we've added a comment here. """
     l = len(word)
     asterisks = "*" * l
     result = text
